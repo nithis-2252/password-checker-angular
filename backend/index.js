861c05918882e30bf33c9e966fd7e3dacd51e2b3
nithis-2252 <gnithisbala@gmail.com>
2026-06-23 14:48:30 +0530

    chore: remove unused Firebase configuration and entrypoint files

--- backend/index.js
+++
@@ -1,5 +0,0 @@
-const functions = require('firebase-functions');
-const app = require('./server');
-
-// Expose Express app as a Single Cloud Function named 'api'
-exports.api = functions.https.onRequest(app);
